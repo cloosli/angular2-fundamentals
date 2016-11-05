79404bf7c17ff2d85f947a5044abb291ad7a3ac7
Christian Loosli <loosli.christian@gmail.com>
2016-11-05 16:12:31 +0100

    Loop Through Angular 2 Components with ngFor

--- src/app/mail.service.ts
+++ src/app/mail.service.ts
@@ -3,8 +3,12 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class MailService {
 
-  message = `You've got mail`
-  
+  messages = [
+    `You've got mail`,
+    `You're now friends with John`,
+    `John liked your tweet`
+  ];
+
   constructor() { }
 
 }
